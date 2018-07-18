b278352379b3870e303442ee453101381a11cb6c
zaucy <ezekiel@robotoppression.com>
2018-07-17 23:29:18 -0700

    Removed np from release script

--- scripts/grpc-gen-release.bat
+++ scripts/grpc-gen-release.bat
@@ -9,9 +9,3 @@ docker run -v %cd%/release:/grpc-web/release --rm -it grpc-gen-protoc-gen-grpc-w
 mv ./release/protoc-gen-grpc-web ./release/protoc-gen-grpc-web-linux
 bazel build :protoc-gen-grpc-web -c opt
 cp ./bazel-out/x64_windows-opt/bin/protoc-gen-grpc-web.exe ./release/protoc-gen-grpc-web-win32.exe
-
-cd grpc-gen-protoc-gen-grpc-web
-np --any-branch
-cd ..
-
-echo "Remember to upload the ./release folder!"
